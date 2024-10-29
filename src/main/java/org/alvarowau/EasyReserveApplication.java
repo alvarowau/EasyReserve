e9e66eb9d373465896448bf641e606b0a5b0645f
alvarowau <alvarobajo893@gmail.com>
2024-10-29 12:01:31 +0100

    Initial commit: set up project structure with MySQL and PostgreSQL configurations, and created environment-specific properties for production, development, and testing.

---
+++ src/main/java/org/alvarowau/EasyReserveApplication.java
@@ -0,0 +1,13 @@
+package org.alvarowau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyReserveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyReserveApplication.class, args);
+    }
+
+}
