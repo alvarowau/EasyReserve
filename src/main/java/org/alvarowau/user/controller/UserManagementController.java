1cf65f2a4cf4ed236cfdbcc194d7a195d5b6c241
AlvaroWau <120642146+alvarowau@users.noreply.github.com>
2024-11-02 10:37:18 +0100

    Refactor: Structure user entities and abstract methods in AbstractBaseUserService
    - Abstracted methods in the AbstractBaseUserService class to improve code reuse.
    - Adjusted logs for user activation and deactivation, now allowing both account activation and deactivation.
    - Implemented the ActionLogs class to keep a record of actions performed.
    - Verified that everything works correctly after these changes.

---
+++ src/main/java/org/alvarowau/user/controller/UserManagementController.java
@@ -0,0 +1,43 @@
+package org.alvarowau.user.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.alvarowau.model.dto.ActionLogRequestAccountStatusChange;
+import org.alvarowau.model.dto.ActionLogResponseAccountStatusChange;
+import org.alvarowau.model.dto.UserAccountStatusChangeRequestByStaff;
+import org.alvarowau.user.service.UserAuthFacade;
+import org.alvarowau.user.service.util.SecurityContextUtil;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/deactivate")
+@RequiredArgsConstructor
+public class UserManagementController {
+
+    private final UserAuthFacade userAuthFacade;
+    private final SecurityContextUtil securityContext;
+
+    @PreAuthorize("hasAnyRole('STAFF', 'CUSTOMER', 'PROVIDER')")
+    @DeleteMapping("/current")
+    public ResponseEntity<ActionLogResponseAccountStatusChange> deactivateCurrentUser(@RequestHeader("Authorization") String token, @RequestBody ActionLogRequestAccountStatusChange requestDelete) {
+        return ResponseEntity.ok(userAuthFacade.deactivateCurrentUser(token, requestDelete));
+    }
+
+    @PreAuthorize("hasAnyRole('STAFF')")
+    @DeleteMapping("/staff/deactivate-user")
+    public ResponseEntity<ActionLogResponseAccountStatusChange> deactivateUserByStaff(
+            @RequestHeader("Authorization") String token,
+            @RequestBody UserAccountStatusChangeRequestByStaff requestDelete) {
+        return ResponseEntity.ok(userAuthFacade.deactivateUserByStaff(token, requestDelete));
+    }
+
+    @PreAuthorize("hasAnyRole('STAFF')")
+    @PostMapping("/staff/activate-user")  // Cambié a POST para la activación
+    public ResponseEntity<ActionLogResponseAccountStatusChange> activateUserByStaff(
+            @RequestHeader("Authorization") String token,
+            @RequestBody UserAccountStatusChangeRequestByStaff requestDelete) {
+        return ResponseEntity.ok(userAuthFacade.activateUserByStaff(token, requestDelete)); // Aquí se asume que 'true' indica activar.
+    }
+}
